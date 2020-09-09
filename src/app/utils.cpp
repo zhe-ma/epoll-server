40952582d002ce0a702e05648a870af3dc1279d3
zhe-ma <mailMazhe@163.com>
2020-09-09 17:30:59 +0800

    Refine code.

--- src/app/utils.cpp
+++ src/app/utils.cpp
@@ -9,6 +9,55 @@
 
 namespace app {
 
+
+namespace socket {
+
+// 设置为非阻塞模式.
+bool EnableNonBlocking(int fd) {
+  int non_blocking = 1;
+  ret = ioctl(fd, FIONBIO, &non_blocking);
+  if (ret == -1) {
+    return false;
+  }
+
+  return true;
+}
+
+bool Bind(int fd, unsigned short port) {
+  struct sockaddr_in sock_addr;
+  memset(&sock_addr, 0, sizeof(sock_addr));
+
+  // AF_INET: IPV4协议族.
+  sock_addr.sin_family = AF_INET;
+  // htonl: 将主机数转换成无符号长整型的网络字节顺序.
+  // INADDR_ANY: 表示一个服务器上所有的网卡的所有个本地IP地址.
+  sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  // htons: 将主机数转换成无符号短整型的网络字节顺序.
+  sock_addr.sin_port = htons(port);
+
+  // 绑定地址.
+  ret = bind(fd, (const struct sockaddr*)&sock_addr, sizeof(sock_addr));
+  if (ret == -1) {
+    return false;
+  }
+
+  return true;
+}
+
+// 打开SO_REUSEADDR选项，防止TIME_WAIT时bind失败的问题.
+bool EnableReuseAddr(int fd) {
+  int reuse_addr = 1;
+  int ret = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void*)&reuse_addr,
+                      sizeof(reuse_addr));
+  if (ret == -1) {
+    return false;
+  }
+
+  return true;
+}
+  
+}  // namespace socket
+
 Socket::Socket()
     : listening_socket_fd_(0) {
 }
@@ -22,19 +71,8 @@ bool Socket::Listen(std::uint16_t port) {
 
   SPDLOG_DEBUG("Port: {}.", port);
 
-  struct sockaddr_in sock_addr;
-  memset(&sock_addr, 0, sizeof(sock_addr));
-
-  // AF_INET: IPV4协议族.
-  sock_addr.sin_family = AF_INET;
-  // htonl: 将主机数转换成无符号长整型的网络字节顺序.
-  // INADDR_ANY: 表示一个服务器上所有的网卡的所有个本地IP地址.
-  sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  // htons: 将主机数转换成无符号短整型的网络字节顺序.
-  sock_addr.sin_port = htons(port);
-
   // SOCK_STREAM: 表示使用TCP连接.
-  listening_socket_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+  listening_socket_fd_ = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0);
   if (listening_socket_fd_ == -1) {
     SPDLOG_ERROR("Failed to create socket.");
     return false;
@@ -50,6 +88,7 @@ bool Socket::Listen(std::uint16_t port) {
     return false;
   }
 
+
   // 设置为非阻塞模式.
   int non_blocking = 1;
   ret = ioctl(listening_socket_fd_, FIONBIO, &non_blocking);
@@ -58,6 +97,17 @@ bool Socket::Listen(std::uint16_t port) {
     Close();
     return false;
   }
+
+  struct sockaddr_in sock_addr;
+  memset(&sock_addr, 0, sizeof(sock_addr));
+
+  // AF_INET: IPV4协议族.
+  sock_addr.sin_family = AF_INET;
+  // htonl: 将主机数转换成无符号长整型的网络字节顺序.
+  // INADDR_ANY: 表示一个服务器上所有的网卡的所有个本地IP地址.
+  sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  // htons: 将主机数转换成无符号短整型的网络字节顺序.
+  sock_addr.sin_port = htons(port);
 
   // 绑定地址.
   ret = bind(listening_socket_fd_, (const struct sockaddr*)&sock_addr, sizeof(sock_addr));
@@ -86,5 +136,6 @@ void Socket::Close() {
     close(listening_socket_fd_);
   }
 }
+
 
 }  // app
---
+++ src/app/utils.h
@@ -0,0 +1,11 @@
+#ifndef APP_UTILS_H_
+#define APP_UTILS_H_
+
+namespace app {
+
+
+
+
+}  // namespace app
+
+#endif  // APP_UTILS_H_
